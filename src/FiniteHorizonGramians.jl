99472779798a57d699dd85e058ded046402e5324
Filip Tronarp <filip.tronarp@matstat.lu.se>
2023-10-11 14:13:58 +0200

    initial commit

--- src/FiniteHorizonGramians.jl
+++ src/FiniteHorizonGramians.jl
@@ -1,5 +1,34 @@
 module FiniteHorizonGramians
 
-# Write your package code here.
+using LinearAlgebra
+using PrecompileTools
+
+include("utils.jl")
+
+abstract type AbstractExpAndGramAlgorithm end
+
+export AbstractExpAndGramAlgorithm
+
+"""
+    exp_and_gram(A::AbstractMatrix, B::AbstractMatrix, alg::AbstractExpAndGramAlgorithm)
+
+Compute the matrix exponential exp(A) and the controllability Gramian of (A, B) over the unit interval.
+"""
+function exp_and_gram end
+
+"""
+    exp_and_gram_chol(A::AbstractMatrix, B::AbstractMatrix, alg::AbstractExpAndGramAlgorithm)
+
+Compute the matrix exponential exp(A) and a upper triangular Cholesky factor of the controllability Gramian of (A, B) over the unit interval.
+"""
+function exp_and_gram_chol end
+
+export exp_and_gram, exp_and_gram_chol
+
+include("exp_and_gram.jl")
+include("adaptive_exp_and_gram.jl")
+export ExpAndGram, AdaptiveExpAndGram
+
+include("precompile.jl")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,29 @@
-using FiniteHorizonGramians
-using Test
+using LinearAlgebra, SpecialFunctions
+using Test, FiniteHorizonGramians
+using ForwardDiff, FiniteDiff
+import FiniteHorizonGramians as FHG
+
+include("test_utils.jl")
+include("test_initial_approximations.jl")
+include("test_adaptive_exp_and_gram.jl")
+include("test_autodiff.jl")
 
 @testset "FiniteHorizonGramians.jl" begin
-    # Write your tests here.
+
+    @testset "initial approximations" begin
+        for T in (Float64,)
+            test_initial_approximation(T)
+        end
+    end
+
+    @testset "adaptive algorithm" begin
+        for T in (Float64,)
+            test_adaptive_exp_and_gram(T)
+        end
+    end
+
+    @testset "autodiff" begin 
+        test_ForwardDiff()
+    end
+
 end
