ba653149f4e09ad92c4d30dea64c9018865cfaef
Filip Tronarp <filip.tronarp@matstat.lu.se>
2023-10-12 14:32:36 +0200

    docstr

--- src/adaptive_exp_and_gram.jl
+++ src/adaptive_exp_and_gram.jl
@@ -1,3 +1,15 @@
+"""
+    AdaptiveExpAndGram{T,A} <: AbstractExpAndGramAlgorithm
+
+Adaptive algorithm for computing the matrix exponential and an associated Gramian. 
+
+Constructor: 
+
+AdaptiveExpAndGram{T}()
+
+creates an algorithm with coefficients stored in the numeric type T. 
+
+"""
 struct AdaptiveExpAndGram{T,A} <: AbstractExpAndGramAlgorithm where {A}
     methods::A
 end
