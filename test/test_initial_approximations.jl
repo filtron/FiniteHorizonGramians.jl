c906d808e6405e3cfbfbe68a99cde954412a3b3b
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-11-10 09:56:49 +0100

    Fix typo in the test that I added

--- test/test_initial_approximations.jl
+++ test/test_initial_approximations.jl
@@ -12,7 +12,7 @@ function test_initial_approximation(T)
                 for ndiff = 1:deg
                     A, B = integrator2AB(T, ndiff)
                     Φgt, Ugt = integrator_exp_and_gram_chol(T, ndiff)
-                    Φ, U = similar(A), similar(U)
+                    Φ, U = similar(A), similar(A)
                     _Φ, _U = FHG._exp_and_gram_chol_init!(Φ, U, A, B, method)
                     @test Φ ≈ _Φ
                     @test U ≈ _U
